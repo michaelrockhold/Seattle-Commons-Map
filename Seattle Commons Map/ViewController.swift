478c0111b8d4a9925b12ebe49effe179604d1b52
Michael Rockhold <michaelrockhold@users.noreply.github.com>
2019-04-24 19:48:56 -0700

    Initial Commit

---
+++ Seattle Commons Map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Seattle Commons Map
+//
+//  Created by Michael Rockhold on 4/24/19.
+//  Copyright © 2019 Michael Rockhold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
